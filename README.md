9a04d6a758b515a712577562a72bfe87d4a1c023
Carlos Patino <carlospatinos@gmail.com>
2021-09-25 22:49:43 +0100

    feat: chatter bot initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hackathon2021
+
+
+# setup
+
+pip3 install chatterbot chatterbot_corpus
---
+++ chatbot.py
@@ -0,0 +1,34 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot(
+    'Buddy',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'
+)
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    'Hi',
+    'Hello',
+    'I need your assistance regarding my order',
+    'Please, Provide me with your order id',
+    'I have a complaint.',
+    'Please elaborate, your concern',
+    'How long it will take to receive an order ?',
+    'An order takes 3-5 Business days to get delivered.',
+    'Okay Thanks',
+    'No Problem! Have a Good Day!'
+])
+
+name = input("Enter Your Name: ")
+print("Welcome to the Bot Service! Let me know how can I help you?")
+while True:
+    request = input(name+':')
+    if request == 'Bye' or request == 'bye':
+        print('Bot: Bye')
+        break
+    else:
+        response = bot.get_response(request)
+        print('Bot:', response)
